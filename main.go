22ac1f09608333ebc324c1a9367b55b904074a8a
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-04 02:21:04 +0900

    円周率

--- main.go
+++ main.go
@@ -2,19 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main(){
-	//Println:そのまま並べる
-	fmt.Println("name:", "Messi", "age:", 38)
-
-	//Printf: フォーマット指定で表示
-	name := "大谷"
-	age := 31
-	height := 193.5
-	fmt.Printf("%sさんは%d歳、身長は%.1fcmです\n", name, age, height)
-
-	// 型の確認(%T)
-	fmt.Printf("name の型: %T\n", name)
-	fmt.Printf("age の型: %T\n", age)
+	// fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+	// fmt.Printf("Now you have %g problems.")
+	fmt.Println(math.Pi)
 }
+
